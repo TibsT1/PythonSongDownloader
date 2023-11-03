8218f3fce20a0933e859f5e335f11b57d9e7c693
TibsT1 <tiberiumtoma@gmail.com>
2023-11-03 20:24:07 +0000

    Gave the app a GUI

--- Main.py
+++ Main.py
@@ -1,32 +1,48 @@
 from pytube import YouTube
 import subprocess
 import os
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
-def convert_video_to_mp3(input_file, output_file):
-    ffmpeg_cmd = [
-        "ffmpeg",
-        "-i", input_file,
-        "-vn",
-        "-acodec", "libmp3lame",
-        "-ab", "128k",
-        "-ar", "44100",
-        "-y",
-        output_file
-    ]
-    try:
-        subprocess.run(ffmpeg_cmd, check=True)
-        print("Conversion Successful")
-    except subprocess.CalledProcessError as e:
-        print("Conversion Failed")
+kivy.require('2.2.0')
 
-url = input("Enter the URL of the video:\n")
+class MyRoot(BoxLayout):
 
-my_video = YouTube(url)
+    def __init__(self):
+        super(MyRoot, self).__init__()
 
-my_video = my_video.streams.get_lowest_resolution()
-my_video.download(filename="song.mp4")
+    def converter(self):
+        def convert_video_to_mp3(input_file, output_file):
+            ffmpeg_cmd = [
+                "ffmpeg",
+                "-i", input_file,
+                "-vn",
+                "-acodec", "libmp3lame",
+                "-ab", "128k",
+                "-ar", "44100",
+                "-y",
+                output_file
+            ]
+            try:
+                subprocess.run(ffmpeg_cmd, check=True)
+                self.url_label.text = str("Conversion Successful")
+            except subprocess.CalledProcessError as e:
+                self.url_label.text = str("Conversion Failed")
 
-output_file_name = my_video.title + ".mp3"
+        my_video = YouTube(self.url_label.text)
 
-convert_video_to_mp3("song.mp4", output_file_name)
-os.remove("song.mp4")
+        my_video = my_video.streams.get_lowest_resolution()
+        my_video.download(filename="song.mp4")
+
+        output_file_name = my_video.title + ".mp3"
+
+        convert_video_to_mp3("song.mp4", output_file_name)
+        os.remove("song.mp4")
+
+class YTdownloader(App):
+    def build(self):
+        return MyRoot()
+
+ytdl = YTdownloader()
+ytdl.run()
